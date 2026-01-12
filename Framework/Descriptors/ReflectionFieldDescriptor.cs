5978b5f99cf97a4067837a25ca939e9f244a59a8
mushymato <wildshinobu@gmail.com>
2026-01-12 00:15:32 -0500

    C#14.0 fix

--- Framework/Descriptors/ReflectionFieldDescriptor.cs
+++ Framework/Descriptors/ReflectionFieldDescriptor.cs
@@ -9,8 +9,8 @@ namespace StardewUI.Framework.Descriptors;
 /// Reflection has the quickest setup time of available methods, but the slowest overall access/write time.
 /// </remarks>
 /// <typeparam name="TValue">The field's value type.</typeparam>
-/// <param name="field">The reflected field.</param>
-public class ReflectionFieldDescriptor<TValue>(FieldInfo field) : IPropertyDescriptor<TValue>
+/// <param name="field">The reflected @field.</param>
+public class ReflectionFieldDescriptor<TValue>(FieldInfo @field) : IPropertyDescriptor<TValue>
 {
     /// <inheritdoc />
     /// <remarks>
@@ -19,10 +19,10 @@ public class ReflectionFieldDescriptor<TValue>(FieldInfo field) : IPropertyDescr
     public bool CanRead => true;
 
     /// <inheritdoc />
-    public bool CanWrite => !field.IsInitOnly && !field.IsLiteral;
+    public bool CanWrite => !@field.IsInitOnly && !@field.IsLiteral;
 
     /// <inheritdoc />
-    public Type DeclaringType => field.DeclaringType!;
+    public Type DeclaringType => @field.DeclaringType!;
 
     /// <inheritdoc />
     public bool IsAutoProperty => false;
@@ -31,15 +31,15 @@ public class ReflectionFieldDescriptor<TValue>(FieldInfo field) : IPropertyDescr
     public bool IsField => true;
 
     /// <inheritdoc />
-    public string Name => field.Name;
+    public string Name => @field.Name;
 
     /// <inheritdoc />
-    public Type ValueType => field.FieldType;
+    public Type ValueType => @field.FieldType;
 
     /// <inheritdoc />
     public object? GetUntypedValue(object source)
     {
-        return field.GetValue(source);
+        return @field.GetValue(source);
     }
 
     /// <inheritdoc />
@@ -51,6 +51,6 @@ public class ReflectionFieldDescriptor<TValue>(FieldInfo field) : IPropertyDescr
     /// <inheritdoc />
     public void SetValue(object target, TValue value)
     {
-        field.SetValue(target, value);
+        @field.SetValue(target, value);
     }
 }
